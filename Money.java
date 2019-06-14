937d777eb7fcf215fbd40cf4a8ba69e3d6a0e1ff
idoaltman <51815384+idoaltman@users.noreply.github.com>
2019-06-14 11:11:36 +0300

    Add files via upload

---
+++ Money.java
@@ -0,0 +1,20 @@
+package com.company;
+public class Money {
+    float value;
+
+    public Money(float value) {
+
+    }
+
+    public void setValue(float value) {
+
+    }
+
+    public float getValue() {
+        return this.value;
+    }
+ // Money
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
