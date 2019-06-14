ea5b3a7f945ff9a977d9d9547c14a817f3296e61
idoaltman <51815384+idoaltman@users.noreply.github.com>
2019-06-14 11:33:27 +0300

    hi

--- Event.java
+++
@@ -1,9 +0,0 @@
-public class Event {
-
-    enum EventType{
-        Party,
-        Birthday,
-        BBQ
-    }
-
-}
---
+++ src/Money.java
@@ -0,0 +1,19 @@
+public class Money {
+    float value;
+
+    public Money(float value) {
+
+    }
+
+    public void setValue(float value) {
+
+    }
+
+    public float getValue() {
+        return this.value;
+    }
+ // Money
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
