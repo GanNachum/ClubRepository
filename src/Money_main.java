7205ed27c258671d1c27d5436ecd4a86ebbe3215
idoaltman <51815384+idoaltman@users.noreply.github.com>
2019-06-14 11:40:10 +0300

    hi

---
+++ src/Money_main.java
@@ -0,0 +1,8 @@
+public class Money_main {
+    public static void main(String[] args) {
+        Money money = new Money(40);
+        System.out.println(money.getValue());
+        money.setValue(50);
+        System.out.println(money.getValue());
+    }
+}
