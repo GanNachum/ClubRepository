05555f93d1a3e5d5661e8cfa623b14052c49d277
oz rots <ozrots0404@gmail.com>
2019-06-14 11:25:42 +0300

    first version

---
+++ src/Date.java
@@ -0,0 +1,55 @@
+package src;
+
+public class Date {
+   private int day,mounth,year ;
+    public Date(){
+
+    }
+    public Date(Date other){
+
+    }
+    public Date(int day,int mounth,int year){
+
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+
+    }
+
+    public int getMounth() {
+        return mounth;
+    }
+
+    public void setMounth(int mounth) {
+
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+
+    }
+
+    public boolean equals(Date other) {
+        boolean result = false;
+        if (this.getDay()==other.getDay() &&  this.getMounth()== other.getMounth() && this.getYear() == other.getYear() ){
+            result=true;
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "src.Date{" +
+                "day=" + this.day +
+                ", mounth=" + this.mounth +
+                ", year=" + this.year +
+                '}';
+    }
+}
---
+++ src/Relationship.java
@@ -0,0 +1,58 @@
+package src;
+
+public class Relationship {
+
+    public enum RelationshipType{
+        InRelationship,
+        Engaged,
+        Married,
+        Divorced,
+        Complex,
+        Widow
+    }
+    private RelationshipType relationshipType;
+    //private Person person1;
+    //private Person person2; // can have null value when unknown
+    private Date startDate; // can have null value when unknown
+    private Date endDate; // can have null value when unknown
+
+    public Relationship(RelationshipType relationshipType, Date startDate, Date endDate) {
+        this.relationshipType = relationshipType;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public RelationshipType getRelationshipType() {
+        return relationshipType;
+    }
+
+    public void setRelationshipType(RelationshipType relationshipType) {
+        this.relationshipType = relationshipType;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Relationship{" +
+                "relationshipType=" + relationshipType +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
+} // Relationship
+
