2f0ff76ce0ad95e2cb31b570a1adb5e28339e733
Maxsim <snasedkin1966@gmail.com>
2019-06-15 20:12:38 +0300

    added datamain

--- src/Date.java
+++ src/Date.java
@@ -1,15 +1,19 @@
-
-
 public class Date {
    private int day,mounth,year ;
     public Date(){
-
+        this.setDay(0);
+        this.setMounth(0);
+        this.setYear(0);
     }
     public Date(Date other){
-
+        this.setMounth(other.getMounth());
+        this.setYear(other.getYear());
+        this.setDay(other.getDay());
     }
     public Date(int day,int mounth,int year){
-
+        this.setDay(day);
+        this.setMounth(mounth);
+        this.setYear(year);
     }
 
     public int getDay() {
@@ -17,6 +21,31 @@ public class Date {
     }
 
     public void setDay(int day) {
+        try {
+            if (mounth==2){
+                if (day<29 && day>-1){
+                    this.mounth= mounth;
+                }
+                else
+                    throw new  IllegalArgumentException();
+            }
+            else if(mounth==1 || mounth==3|| mounth == 5 || mounth == 7 ||mounth == 8||mounth == 10||mounth == 12 ){
+                if (day<32 && day>-1){
+                    this.mounth= mounth;
+                }
+                else
+                    throw new  IllegalArgumentException();
+            }
+            else {
+                if (day < 32 && day > -1) {
+                    this.mounth = mounth;
+                } else
+                    throw new  IllegalArgumentException();
+            }
+        }catch (Exception e){
+            e=null;
+            System.out.println("illegal argument");
+        }
 
     }
 
@@ -25,6 +54,17 @@ public class Date {
     }
 
     public void setMounth(int mounth) {
+        try {
+            if (mounth<13 && mounth>-1){
+                this.mounth= mounth;
+            }
+            else
+                throw new  IllegalArgumentException();
+        }catch (Exception e){
+            e=null;
+            System.out.println("illegal argument");
+        }
+
 
     }
 
@@ -33,7 +73,7 @@ public class Date {
     }
 
     public void setYear(int year) {
-
+        this.year=year;
     }
 
     public boolean equals(Date other) {
---
+++ src/DateMain.java
@@ -0,0 +1,11 @@
+public class DateMain {
+    public static  void main(String args[]){
+        Date date2= new Date();
+        Date date4= new Date(date2);
+        Date date3= new Date(13,07,2019);
+        if(date2.equals(date4));{
+            System.out.println("equals");
+        }
+        System.out.println(date3.toString());
+    }
+}
