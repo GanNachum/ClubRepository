1b2045b362a5384d963a7efdd8ae1d19e06cdfe1
ronex2003 <ron.rozenman@gmail.com>
2019-06-16 08:40:11 +0300

    Loaction class

---
+++ src/Location.java
@@ -0,0 +1,13 @@
+class Location {
+    GPS gps;
+    Country country;
+    State state;
+    City city;
+    Street street;
+    Building building;
+    string doorCode;
+    int floor;
+    Apartment apartment;
+    Zip	zip;
+    Venue venue;
+}
